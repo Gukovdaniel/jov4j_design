b7b5dcb0fcc329ee6dfb935173bcf00e9c762f49
gukov <gukov.danila@bk.ru>
2023-01-31 19:38:43 +0300

    Iterator for even numbers

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -5,23 +5,17 @@ import java.util.NoSuchElementException;
 
 public class EvenNumbersIterator implements Iterator<Integer> {
     private int[] data;
-    private int index;
+    private int index = 0;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
     }
 
     public boolean hasNext() {
-        index = 0;
-        while (index < data.length) {
-            if (data[index] % 2 == 0) {
-                return true;
-            } else {
-                index++;
-                continue;
+        while (index < (data.length - 1) && (data[index] % 2) != 0) {
+            index++;
             }
-        }
-        return false;
+        return index++ < data.length;
     }
 
     public Integer next() throws NoSuchElementException {
@@ -30,4 +24,12 @@ public class EvenNumbersIterator implements Iterator<Integer> {
         }
         return data[index];
     }
+    /**
+     * В заголовке цикла while разместите условия -
+     * что индекс не вышел за границу массива и что
+     * элемент по этому индексу нечетный.
+     * Если это так, то в теле цикла перейдите на следующий элемент,
+     * если нет - то выйдите из цикла и в операторе return проверьте,
+     * что индекс не вышел за границу массива.
+     */
 }
