9eaf1b4b261ca36a4db1b7c6845275d18ee15e5d
gukov <gukov.danila@bk.ru>
2023-02-03 14:10:31 +0300

    Create a container based on a linked list

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,76 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, new Node<>(value, head));
+        if (this.head == null) {
+           this.head = newNode.next;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+            this.size++;
+        this.modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> rsl = this.head;
+        for (int i = 0; i < index; i++) {
+            rsl = rsl.next;
+        }
+        return head.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int expectedModCount = modCount;
+            Node<E> current = null;
+            Node<E> prev = null;
+            Node<E> ensuing = head;
+
+            @Override
+            public boolean hasNext() {
+                checkModCount();
+                return ensuing != null;
+            }
+
+            @Override
+            public E next() {
+                checkModCount();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                prev = current;
+                current = ensuing;
+                ensuing = ensuing.next;
+                return null;
+            }
+
+            private void checkModCount() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
+    }
+}
