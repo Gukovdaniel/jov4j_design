d410442c12b06abe6ff321dc7b2b96b5ca41c67a
gukov <gukov.danila@bk.ru>
2023-02-17 13:42:03 +0300

    FileOutputStream

--- src/main/java/ru/job4j/io/FileFile.java
+++ src/main/java/ru/job4j/io/FileFile.java
@@ -1,16 +1,23 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class FileFile {
+
+
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-            out.write(String.valueOf(multiple(9)).getBytes());
-            out.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String s = Arrays.deepToString(multiple(4));
+            try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+                out.write(s.getBytes());
+                out.write(System.lineSeparator().getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
+
     public static int[][] multiple(int size) {
         int[][] multiple = new int[size][size];
         for (int i = 0; i < size - 1; i++) {
