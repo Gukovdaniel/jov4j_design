ac0c56825d986a423ee3c007bf681d753e54f354
gukov <gukov.danila@bk.ru>
2023-02-19 10:54:04 +0300

    EvenNumbers

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("data/even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+           String even = String.valueOf(text);
+            int[] numArr = Arrays.stream(even.split("\r\n")).mapToInt(Integer::parseInt).toArray();
+            for (int temp : numArr) {
+                if (temp % 2 == 0) {
+                    System.out.println(temp);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
