1511440e92e231ab5f93d3a76ff4b6174e7ffcbd
gukov <gukov.danila@bk.ru>
2023-02-19 12:32:12 +0300

    FileInputStream

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -19,7 +20,7 @@ public class EvenNumberFile {
                     System.out.println(temp);
                 }
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
