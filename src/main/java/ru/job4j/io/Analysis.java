ff174a88530cb623f5ef65597380d8c822b7d2db
gukov <gukov.danila@bk.ru>
2023-02-24 10:13:19 +0300

    Анализ доступности сервера

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,34 +1,26 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Analysis {
 
     public void unavailable(String source, String target) {
      try (BufferedReader br = new BufferedReader(new FileReader(source));
           PrintWriter pr = new PrintWriter(new FileOutputStream(target))) {
-         List<String> list = new ArrayList<>();
          String c;
-         String start = null;
-         String end = null;
+         boolean flag = false;
+         boolean flagStart = false;
          while ((c = br.readLine()) != null) {
-             if (start == null && (c.startsWith("400") || c.startsWith("500"))) {
+             if (!flag && (c.startsWith("400") || c.startsWith("500"))) {
                  String[] split = c.split(" ");
-                 start = split[1] + ";";
-             } else if (start != null && !(c.startsWith("400") || c.startsWith("500"))) {
+                 pr.print(split[1] + ";");
+                 flag = true;
+                 flagStart = true;
+             } else if (flagStart && !(c.startsWith("400") || c.startsWith("500"))) {
                  String[] split = c.split(" ");
-                 end = split[1];
-             }
-             if (start != null && end != null) {
-                 pr.print(start);
-                 pr.print(end + System.lineSeparator());
-                 start = null;
-                 end = null;
+                 pr.print(split[1]);
+                 pr.print(System.lineSeparator());
+                 flag = false;
              }
          }
      } catch (IOException e) {
