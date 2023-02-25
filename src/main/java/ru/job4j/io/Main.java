879abb6c665a2cb2924f8e495aae991fe0dc9012
gukov <gukov.danila@bk.ru>
2023-02-25 11:15:16 +0300

    Файлы и директории. Класс File

---
+++ src/main/java/ru/job4j/io/Main.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Main {
+    public static final String PATH = "New folder";
+
+        public static void main(String[] args) throws IOException {
+            File dir = new File("src/main/java/ru/job4j/io/files/directory");
+            dir.mkdirs();
+            File target = new File("src/main/java/ru/job4j/io/files");
+            File file1 = new File("src/main/java/ru/job4j/io/files/file1.txt");
+            file1.createNewFile();
+            File file2 = new File("src/main/java/ru/job4j/io/files/directory/file2.txt");
+            file2.createNewFile();
+            String[] list = target.list();
+            for (String f : list) {
+                System.out.println(f);
+            }
+            File[] listFiles = target.listFiles();
+            for (File f : listFiles) {
+                System.out.println(f);
+            }
+        }
+}
+
