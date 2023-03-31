dbab5b7401a82cb3f763b9fb8b748b481f8dd639
gukov <gukov.danila@bk.ru>
2023-03-31 11:58:50 +0300

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ArgsName {
+
     private final Map<String, String> values = new HashMap<>();
 
     public String get(String key) {
--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,10 +3,10 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
 
 public class Zip {
     public void packFiles(List<File> sources, File target) {
@@ -36,18 +36,33 @@ public class Zip {
     }
 
     public static void main(String[] args) {
-        Search search = new Search();
+        if (args.length != 3) {
+            throw new IllegalArgumentException();
+        }
+        ArgsName argsName = ArgsName.of(args);
+        validate(argsName);
+        List<Path> listPaths = new ArrayList<>();
         try {
-            search.main(args);
+            listPaths = Search.search(Path.of(args[0]),
+                    p -> !p.toFile()
+                            .getName()
+                            .endsWith(args[1]));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ArgsName argsName = ArgsName.of(args);
-        argsName.main(args);
-        Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
+        List<File> listFiles = new ArrayList<>();
+        for (Path path : listPaths) {
+            listFiles.add(path.toFile());
+        }
+        new Zip().packFiles(listFiles, new File(argsName.get("o")));
+    }
+
+    private static void validate(ArgsName argsName) {
+        if (!argsName.get("e").endsWith(".class")) {
+            System.out.println("Неправильный ввод параметра класса");
+        }
+        if (!argsName.get("o").endsWith(".zip")) {
+            System.out.println("Неправильный ввод создаваемого архива");
+        }
     }
 }
