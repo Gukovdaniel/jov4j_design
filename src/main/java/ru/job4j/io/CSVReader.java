52f90a0e586d7c6dc9a29921d63fed13f4b1ac48
gukov <gukov.danila@bk.ru>
2023-04-12 15:20:58 +0300

    csv reader

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -8,30 +8,30 @@ public class CSVReader {
         File file = new File(argsName.get("path"));
         ArrayList<String> nameAge = new ArrayList<>();
         try (Scanner scanner = new Scanner(file)) {
-            scanner.useDelimiter(argsName.get("delimiter")); // задаем разделитель значений
+            scanner.useDelimiter(argsName.get("delimiter"));
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] fields = line.split(argsName.get("delimiter"));
-                String name = fields[0];
-                String age = fields[1];
-                if (argsName.get("out").contains("stdout")) {
-                    System.out.println("Name: " + name + ", Age: " + age);
-                } else if (file.exists() && !file.isDirectory()) {
-                   nameAge.add(name);
-                   nameAge.add(age);
-                }
-
+                nameAge.add(fields[0]);
+                nameAge.add(fields[1]);
             }
         }
         conclusion(file, nameAge.get(0), nameAge.get(1));
     }
 
-    private static void conclusion(File file, String name, String age) {
-        try (FileOutputStream fos = new FileOutputStream(file)) {
-            fos.write(Integer.parseInt("Name: " + name + ", Age: " + age));
-            System.out.println("Data written to file.");
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static void conclusion(File file, String name, String age) throws IOException {
+        if (name.contains("stdout")) {
+            System.out.println("Name: " + name + ", Age: " + age);
+        } else {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                fos.write(("Name: " + name + ", Age: " + age).getBytes());
+                System.out.println("Data written to file.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -42,12 +42,6 @@ public class CSVReader {
         ArgsName argsName = ArgsName.of(args);
         val(argsName);
         handle(argsName);
-
-        /**
-         * Печать результата в консоль надо выполнять,
-         * только если по параметру out передано stdout, а если передано другое -
-         * то считайте, что передано имя файла, который надо создать и вывести результат в него.
-         */
     }
 
     private static void val(ArgsName args) {
@@ -55,14 +49,14 @@ public class CSVReader {
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
-        if (args.get("path").endsWith(".csv")) {
+        if (!args.get("path").endsWith(".csv")) {
             System.out.println("Неправильный формат");
         }
         if ((args.get("delimiter").length() > 1)) {
             throw new IllegalArgumentException("Проблема с втрорым параметром. Тут нужно указать разделитель");
         }
         File file1 = new File(args.get("out"));
-        if (!file1.exists() && file1.isDirectory()) {
+        if (!file1.exists() || file1.isDirectory()) {
             throw new IllegalArgumentException("Проблема с третьим параметром. Тут нужно указать куда мы сокраняем");
         }
         if (!args.get("filter").contains("name") && !args.get("filter").contains("age")) {
