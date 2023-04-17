c4b6812ee9077ecf9ba4fe12cd9452900faba886
gukov <gukov.danila@bk.ru>
2023-04-17 10:32:59 +0300

    EchoServer

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,17 +6,20 @@ import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(8000)) {
+        try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("HTTP/1.1 200 Ready\r\n\r\n".getBytes());
                     String requestLine = in.readLine();
-                    if (requestLine != null && requestLine.contains("msg=Bye")) {
+                    if (requestLine != null && requestLine.contains("msg=Hello")) {
+                        out.write("HTTP/1.1 200 Hello\r\n\r\n".getBytes());
+                    } else if (requestLine != null && requestLine.contains("msg=Exit")) {
                         server.close();
                     } else {
+                        out.write(Integer.parseInt("HTTP/1.1 200 " + requestLine + "\r\n\r\n".getBytes()));
                         for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                             System.out.println(str);
                         }
